6fb983caef01861994aac0fd2e536f3e39bed98b
Julian Mura-Kröger <jakrog14@gmail.com>
2026-04-16 15:09:26 -0400

    Fixed mapping portion in html

--- scripts.js
+++ scripts.js
@@ -2,9 +2,9 @@
 mapboxgl.accessToken = 'MAPBOX_TOKEN_PLACEHOLDER';
 const map = new mapboxgl.Map({
     container: 'map_container',
-    style: 'mapbox://styles/mapbox/standard', // Using standard style for map
-    projection: 'globe', // need to change this!
-    zoom: 10, // initial zoom 10 for now, will need to change
-    center: [-73.91658, 40.69540] // centering the map on CD304
+    style: 'mapbox://styles/mapbox/standard', // Use the standard style for the map
+    projection: 'globe', // display the map as a globe
+    zoom: 10, // initial zoom level, 0 is the world view, higher values zoom in
+    center: [-73.91658, 40.69540] // center the map on this longitude and latitude
 });
 
